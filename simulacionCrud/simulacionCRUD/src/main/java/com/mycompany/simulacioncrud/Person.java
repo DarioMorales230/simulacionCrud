f731366b1134c7d0b75ae2188e167c674cbfd029
mauro1012 <85144847+DarioMorales230@users.noreply.github.com>
2025-01-22 00:15:22 -0500

    Lógica clase persona
    Creemos los constructores, getter and setter para el manejo de información de la clase

--- simulacionCrud/simulacionCRUD/src/main/java/com/mycompany/simulacioncrud/Person.java
+++ simulacionCrud/simulacionCRUD/src/main/java/com/mycompany/simulacioncrud/Person.java
@@ -9,5 +9,55 @@ package com.mycompany.simulacioncrud;
  * @author User
  */
 public class Person {
+    private String name,lastname, cedula, phone;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Person(String name, String lastname, String cedula, String phone, int age) {
+        this.name = name;
+        this.lastname = lastname;
+        this.cedula = cedula;
+        this.phone = phone;
+        this.age = age;
+    }
     
 }
