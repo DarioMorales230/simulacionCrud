1456321f8be2f932f157242f50bf884030199ad9
mauro1012 <85144847+DarioMorales230@users.noreply.github.com>
2025-01-21 23:57:42 -0500

    cambios clase main
    aumentamos codigo para que se pueda ver la ubicacion de los botones al momento de ejecutar el programa para ver si toca cambiar .

--- simulacionCrud/simulacionCRUD/src/main/java/com/mycompany/simulacioncrud/MainMenu.java
+++ simulacionCrud/simulacionCRUD/src/main/java/com/mycompany/simulacioncrud/MainMenu.java
@@ -18,12 +18,30 @@ public class MainMenu extends JFrame {
         JButton btnUpdate = new JButton("Actualizar");
         JButton btnDelete = new JButton("Eliminar");
 
-        // Configuración de posición y tamaño de los botones
+        // Configuración de ubicacion y tamaño de los botones
         btnCreate.setBounds(140, 30, 120, 30);
         btnRead.setBounds(140, 80, 120, 30);
         btnUpdate.setBounds(140, 130, 120, 30);
         btnDelete.setBounds(140, 180, 120, 30);
+        
+        // Agregar los botones a la ventana principal
+        add(btnCreate);
+        add(btnRead);
+        add(btnUpdate);
+        add(btnDelete);
 
+    }
+    
+    // metodo para abrir un JinternalFrame dentro del JFrame principal
+    
+    private void openInternalJframe(JInternalFrame frame){
+        JDesktopPane desktopPane = new JDesktopPane();
+        //nos ayuda a configurar el contenido del JFrame para incluir el JDesktopPnae
+        setContentPane(desktopPane);
+        //agrega elJInternalFrame al JDesktopPane
+        desktopPane.add(frame);
+        // nos ayuda a que este visible el Jinternalframe
+        frame.setVisible(true);
     }
 
 
